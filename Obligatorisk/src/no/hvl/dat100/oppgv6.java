4f468617f9cf612c1db1c1dcdd2266b6cbcc97b7
h593274 <70374371+h593274@users.noreply.github.com>
2020-10-20 14:39:06 +0200

    Update oppgv6.java

--- Obligatorisk/src/no/hvl/dat100/oppgv6.java
+++ Obligatorisk/src/no/hvl/dat100/oppgv6.java
@@ -1,5 +1,17 @@
-package no.hvl.dat100;
+package no.hvl.dat100.idris;
 
 public class oppgv6 {
+	
+public static void main(String[] args) {
+System.out.println(fakultet(4));
+}
 
+public static int fakultet(int n) {
+	if(n==1) {
+		return 1;
+	}
+	else {
+		return n*fakultet(n-1);
+	}
+}
 }
